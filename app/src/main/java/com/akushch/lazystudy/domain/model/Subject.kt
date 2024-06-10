8ea09cb3da04c7db5d14d10213c318c46f7c8583
Artem Kushch <artemkusch2@gmail.com>
2024-06-11 02:44:12 +0300

    Add Room implementation to save the data

--- app/src/main/java/com/akushch/lazystudy/domain/model/Subject.kt
+++ app/src/main/java/com/akushch/lazystudy/domain/model/Subject.kt
@@ -6,12 +6,16 @@ import com.akushch.lazystudy.ui.theme.gradient2
 import com.akushch.lazystudy.ui.theme.gradient3
 import com.akushch.lazystudy.ui.theme.gradient4
 import com.akushch.lazystudy.ui.theme.gradient5
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity
 data class Subject(
     val name: String,
     val goalHours: Float,
     val colors: List<Color>,
-    val subjectId: Int
+    @PrimaryKey(autoGenerate = true)
+    val subjectId: Int? = null
 ) {
     companion object {
         val subjectCardColors = listOf(gradient1, gradient2, gradient3, gradient4, gradient5)
